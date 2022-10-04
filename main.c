7aae6fc3b3623dccc4b020a0feebd96ca60130dd
Guilhermo Masid <guilhermomasid@gmail.com>
2022-10-04 17:51:21 -0300

    chore: send to test in not m1 mac

--- main.c
+++ main.c
@@ -1,6 +1,45 @@
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define THREADS_NUM 40
+
+int				shared = 0;
+
+pthread_mutex_t	mutex;
+
+void	thread_function(int n)
+{
+	int	a;
+	int	b;
+
+	for (int i = 0; i < 100000; i++)
+	{
+		pthread_mutex_lock(&mutex);
+		// We're fooling around to pretend that an variable is accessing the data
+		a = shared;
+		b = 1;
+		shared = a + b;
+		pthread_mutex_unlock(&mutex);
+	}
+}
 
 int	main(void)
 {
-	printf("Here we go again");
+	pthread_t	threads[THREADS_NUM];
+
+	for (int i = 0; i < THREADS_NUM; i++)
+	{
+		// Create thread
+		pthread_create(&threads[i], NULL, (void *)thread_function, NULL);
+	}
+	for (int i = 0; i < THREADS_NUM; i++)
+	{
+		// Wait threads to finish
+		pthread_join(threads[i], NULL);
+	}
+	// Expected to be 2.000.000
+	printf("shared = %d\n", shared);
+	return (0);
 }
