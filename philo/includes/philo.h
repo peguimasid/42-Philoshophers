70e7181bb983ee4ba66b2f8b0bed5d7103e4aca2
Guilhermo Masid <guilhermomasid@gmail.com>
2022-10-05 17:50:08 -0300

    feature: handle send wrong num of args

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 15:16:25 by gmasid            #+#    #+#             */
-/*   Updated: 2022/10/05 15:18:13 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/10/05 17:42:00 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,14 @@
 
 typedef struct s_info
 {
-	int	philo_amount;
+	int	num_of_philo;
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
-	int	must_eat_amount;
+	int	num_times_must_eat;
 }		t_info;
+
+int		throw_error(char *msg);
+int		print_usage(void);
 
 #endif
--- philo/src/common.c
+++ philo/src/common.c
@@ -1,20 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   common.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/05 15:18:23 by gmasid            #+#    #+#             */
-/*   Updated: 2022/10/05 17:42:39 by gmasid           ###   ########.fr       */
+/*   Created: 2022/10/05 17:32:11 by gmasid            #+#    #+#             */
+/*   Updated: 2022/10/05 17:43:34 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-int	main(int argc, char **argv)
+int	print_usage(void)
 {
-	printf("%c", argv[0][0]);
-	if (argc != 5 && argc != 6)
-		return (print_usage());
+	return (throw_error("Usage: ./philo <num_of_philo> <time_to_die> <time_to_eat> \
+<time_to_sleep> [num_times_philo_should_sleep]"));
+}
+
+int	throw_error(char *msg)
+{
+	int	i;
+
+	i = 0;
+	while (msg[i])
+		i++;
+	write(STDERR_FILENO, "Error\n", 6);
+	write(STDERR_FILENO, msg, i);
+	write(STDERR_FILENO, "\n", 1);
+	return (1);
 }
