3bb17f44adf5c1f22a398a56cd271fa14c9f293f
Guilhermo Masid <guilhermomasid@gmail.com>
2022-10-05 13:53:13 -0300

    feature: create header file

---
+++ philo/includes/philo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/13 17:13:18 by gmasid            #+#    #+#             */
+/*   Updated: 2022/10/05 13:51:17 by gmasid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -1,7 +1,4 @@
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "../includes/philo.h"
 
 #define THREADS_NUM 40
 
@@ -32,15 +29,9 @@ int	main(void)
 
 	pthread_mutex_init(&mutex, NULL);
 	for (int i = 0; i < THREADS_NUM; i++)
-	{
-		// Create thread
 		pthread_create(&threads[i], NULL, (void *)thread_function, (void *)2);
-	}
 	for (int i = 0; i < THREADS_NUM; i++)
-	{
-		// Wait threads to finish
 		pthread_join(threads[i], NULL);
-	}
 	// Expected to be 4.000.000
 	printf("shared = %d\n", shared);
 	return (0);
