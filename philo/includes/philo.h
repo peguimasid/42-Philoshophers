2b612b42f28d66bc08e2489a1b63705bc222efbf
Guilhermo Masid <guilhermomasid@gmail.com>
2022-10-08 12:35:51 -0300

    feature: add philo struct

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 15:16:25 by gmasid            #+#    #+#             */
-/*   Updated: 2022/10/06 15:40:24 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/10/08 12:35:31 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,31 @@
 # include <string.h>
 # include <unistd.h>
 
+typedef struct s_philo
+{
+	int				id;
+	pthread_mutex_t	left;
+	pthread_mutex_t	right;
+	pthread_t		thread;
+}					t_philo;
+
 typedef struct s_info
 {
-	int	num_of_philo;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	num_times_must_eat;
-}		t_info;
+	int				num_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				num_times_must_eat;
+	t_philo			*philos;
+}					t_info;
 
-void	init_args(t_info *info, int argc, char **argv);
-int		is_valid_args(int argc, char **argv);
+void				init_args(t_info *info, int argc, char **argv);
+int					is_valid_args(int argc, char **argv);
 
-int		throw_error(char *error);
-int		print_usage(void);
+int					throw_error(char *error);
+int					print_usage(void);
 
-int		ft_atoi(char *str);
-int		ft_isnum(char *str);
+int					ft_atoi(char *str);
+int					ft_isnum(char *str);
 
 #endif
