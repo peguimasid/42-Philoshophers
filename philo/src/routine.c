e9f8c362e0eade1425f52681754933c867e63158
Guilhermo Masid <guilhermomasid@gmail.com>
2022-10-09 11:17:11 -0300

    chore: delay philosophers with odd ids

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 23:07:02 by gmasid            #+#    #+#             */
-/*   Updated: 2022/10/09 11:02:55 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/10/09 11:13:21 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,17 @@ void	*routine(void *argv)
 	int		right_fork;
 
 	philo = argv;
-	pthread_mutex_lock(&philo->global->forks[0]);
-	usleep(1000000);
-	printf("philosopher = %d =>", philo->id);
+	if (philo->id % 2)
+		usleep(philo->global->time_to_eat * 1000);
 	left_fork = philo->id - 1;
 	right_fork = philo->id % philo->global->num_of_philo;
+	pthread_mutex_lock(&philo->global->forks[left_fork]);
+	pthread_mutex_lock(&philo->global->forks[right_fork]);
+	usleep(1000000);
+	printf("philosopher = %d =>", philo->id);
 	printf(" left fork = %d | right fork = %d\n", left_fork, right_fork);
 	printf("run after %lums\n", current_time(philo));
-	pthread_mutex_unlock(&philo->global->forks[0]);
+	pthread_mutex_unlock(&philo->global->forks[left_fork]);
+	pthread_mutex_unlock(&philo->global->forks[right_fork]);
 	return (NULL);
 }
